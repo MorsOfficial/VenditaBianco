dac8a5c05a49071bce90d5def8bd47156a397c93
Maurizio <morsellimaurizio@istitutocorni.it>
2022-03-15 22:33:31 +0100

    vandelli zebbt

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          AA <code>src/App.js</code> and save to reload.
+          Vandelli zeebby <code>src/App.js</code> and save to reload.
         </p>
         <a
           className="App-link"
